81b979b455d05acae9811cd6d295c0a3069280d1
Kamil <isaevkamil029@mail.ru>
2022-10-08 22:43:25 +0300

    fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-
 import Button from "../Button/Button";
-
+import { useTelegram } from "../../hooks/useTelegram";
 
 const Header = () => {
     const {user, onClose} = useTelegram();
