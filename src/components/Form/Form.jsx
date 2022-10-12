35087520247e1618f14e52cfa45e5eddbc373019
Kamil <isaevkamil029@mail.ru>
2022-10-12 11:48:21 +0300

    fix

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './Form.css';
 import {useTelegram} from "../../hooks/useTelegram";
 
